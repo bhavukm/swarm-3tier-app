68c26112de412d49aaa6646a4affac787dbb37f3
root <root@ip-172-31-19-78.ec2.internal>
2025-05-03 07:29:47 +0000

    swarm-3tier-app

---
+++ be/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from Docker Swarm backend!' });
+});
+app.listen(3000, () => console.log('Backend running on port 3000'));
+
